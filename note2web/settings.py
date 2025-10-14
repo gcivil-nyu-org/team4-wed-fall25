7a393b1a3f397c8abf900b197cb5fcd5aeefc8d3
Dhruv Gupta <dhruvgupta@10-20-95-79.dynapool.wireless.nyu.edu>
2025-10-14 18:15:51 -0400

    added changes

--- note2web/settings.py
+++ note2web/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "note2web.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "NAME": os.path.join(tempfile.gettempdir(), "db.sqlite3"),
     }
 }
 
