a8667f22a46a6d5464b2b8da82d8a36063e14ba8
Dhruv Gupta <dhruvgupta@10-20-95-79.dynapool.wireless.nyu.edu>
2025-10-21 19:46:07 -0400

    Add Travis CI with Black and Flake8 configuration

--- note2web/settings.py
+++ note2web/settings.py
@@ -27,7 +27,9 @@ SECRET_KEY = "django-insecure-&o)&&#rx8x52xt-^45hwuzf*86x69zhy^bvh2z#q^nl%$61*%&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*",]
+ALLOWED_HOSTS = [
+    "*",
+]
 
 
 # Application definition
@@ -85,7 +87,6 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -120,10 +121,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "/static/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_URL = "/media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
