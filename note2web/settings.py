5d21aa2328af7f492f73b49716982e2beba63902
Aayush Shah <aayushshah22@gmail.com>
2025-10-30 12:21:27 -0400

    Implement password reset flow and verify SendGrid integration

--- note2web/settings.py
+++ note2web/settings.py
@@ -131,15 +131,12 @@ MEDIA_URL = "/media/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
-# TODO For prod, switch later:
-# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-# EMAIL_HOST = "smtp.gmail.com"
-# EMAIL_PORT = 587
-# EMAIL_USE_TLS = True
-# EMAIL_HOST_USER = "your_email@gmail.com"
-# EMAIL_HOST_PASSWORD = "your_app_password"
-
-# Where password reset emails appear to come from
-DEFAULT_FROM_EMAIL = "webmaster@localhost"
+# --- CRUCIAL ---
+EMAIL_HOST_USER = "apikey"  # literally the string "apikey", do NOT change this
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL")
