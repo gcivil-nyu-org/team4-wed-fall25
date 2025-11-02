64badb9cb9865f22b7ff6e69599f704b9f2a60b4
Dhruv Gupta <dhruvgupta@Mac-678.lan>
2025-11-02 02:24:59 -0500

    Enhcnae UI changes for uploader and reviewer screens

--- note2webapp/urls.py
+++ note2webapp/urls.py
@@ -31,4 +31,5 @@ urlpatterns = [
     path("delete-model/<int:model_id>/", views.delete_model, name="delete_model"),
     path("reviewer/", views.reviewer_dashboard, name="reviewer_dashboard"),
     path("test-model/<int:version_id>/", views.test_model_cpu, name="test_model_cpu"),
+    path('version/<int:version_id>/edit-information/', views.edit_version_information, name='edit_version_information'),
 ]
