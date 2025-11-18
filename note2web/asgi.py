e23b26c1aec21e756b22b73d61cca7de71a43b66
yujipranav <100519024+yujipranav@users.noreply.github.com>
2025-11-17 21:53:07 -0500

    Update asgi.py

--- note2web/asgi.py
+++ note2web/asgi.py
@@ -13,7 +13,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "note2web.settings")
 
 # Initialize Django ASGI application early to ensure the AppRegistry
 # is populated before importing code that may import ORM models.
-from django.core.asgi import get_asgi_application
+from django.core.asgi import get_asgi_application  # noqa: E402
 
 django_asgi_app = get_asgi_application()
 
